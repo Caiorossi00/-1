f0c52bc053dcc25560caf791164fc22501d4716a
Caiorossi00 <caio_rossi56@hotmail.com>
2024-11-09 10:22:16 -0300

    ui

--- components/Item.js
+++ components/Item.js
@@ -1,41 +1,62 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
 
 const Container = styled.View`
-  padding: 10px;
+  padding: 15px;
   border-width: 1px;
   border-color: #ccc;
   border-radius: 5px;
   background-color: #fff;
-  display:flex;
   flex-direction: row;
-  gap: 3em;
+  align-items: center;
+  justify-content: space-between;
+  width: 300px;
+`;
+
+const InfoContainer = styled.View`
+  flex-direction: column;
 `;
 
 const ItemText = styled.Text`
-  font-size: 18px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const TimeText = styled.Text`
+  font-size: 12px;
+  color: #555;
 `;
 
 const DaysText = styled.Text`
-  font-size: 16px;
+  font-size: 32px;
+  font-weight: bold;
 `;
 
-const ButtonContainer = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: #f0f0f0;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border-width: 1px;
+  border-color: #ccc;
+  margin-top: 5px;
 `;
 
-const Button = styled.Button``;
+const ButtonText = styled.Text`
+  font-size: 14px;
+  color: #333;
+`;
 
 const Item = ({ item, incrementDays, deleteItem }) => {
   return (
     <Container>
-      <ItemText>{item.name}</ItemText>
-      <DaysText>{item.days} dias</DaysText>
-      <ButtonContainer>
-        <Button title="+1 Dia" onPress={() => incrementDays(item.id)} />
-        <Button title="Excluir" onPress={() => deleteItem(item.id)} />
-      </ButtonContainer>
+      <InfoContainer>
+        <ItemText>{item.name}</ItemText>
+        <TimeText>{item.time}</TimeText>
+        <ButtonContainer onPress={() => incrementDays(item.id)}>
+          <ButtonText>+1</ButtonText>
+        </ButtonContainer>
+      </InfoContainer>
+      <DaysText>{item.days}</DaysText>
     </Container>
   );
 };
